04eeb27d6d5fb5677d10b63328a9178a6da59a48
cicly1 <44709978+cicly1@users.noreply.github.com>
2018-11-12 22:54:01 +0000

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 //mostrar imagenes y usar sass
 // @ts-ignore
-var logoImg = require("../imagenes/logo_1.png");
+var logoIm = require("../imagenes/logo_1.png");
 const img = document.createElement('img');
-img.src = logoImg;
+img.src = logoIm;
 document.getElementById('imgContainer').appendChild(img);
